cf127e13118a0576929b5d3c4dd985b7a72f810f
Piotr Kontowicz <przemyslowiec@gmail.com>
2019-05-23 20:07:46 +0200

    Dataset generation update.

--- datasetGeneration.py
+++ datasetGeneration.py
@@ -1,163 +1,125 @@
 from PIL import Image, ImageDraw, ImageFont
-import string
-import ntpath
-import numpy as np
-import os
 import glob
 import os
-import random
 import cv2
 import numpy as np
-import re
 
-random.seed(2)
+def addPadding(img, targetSize, percent = 30):
+    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    height = []
+    for i in range(0, len(img)):
+        if 0 in img[i]:
+            height.append(i)
 
-def blur(img):
-   return cv2.blur(img, (5,5))
+    width = []
+    for i in range(0, len(img)):
+        tmp = [row[i] for row in img]
+        if 0 in tmp:
+            width.append(i)
 
-def gaussian(img):
-   return cv2.GaussianBlur(img, (5,5), 0)
+    minX = min(height)
+    maxX = max(height)
 
-def median(img):
-   return cv2.medianBlur(img,5)
+    minY = min(width)
+    maxY = max(width)
 
-def generateBasicDataset():
-    fontSize = 40
-    imgSize = (50,50)
-    position = (0,0)
-    
-    dataset_path = os.path.join (os.getcwd(), 'Synthetic_dataset')
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path)
-    
-    fhandle = open('Fonts_list.txt', 'r')
+    newImg = img[minX: maxX + 2, minY:maxY + 2]
+
+    w, h = newImg.shape[:2]
+    p = int(h + 2 * h * percent / 100) // 2
+
+    color = [255, 255, 255]
+    img =  cv2.copyMakeBorder(newImg, p, p, p, p, cv2.BORDER_CONSTANT, value=color)
+    return cv2.resize(img, (targetSize, targetSize))
+
+def gausiabBlur(img, size = 3):
+    return cv2.GaussianBlur(img,(size, size), 0)
+
+def avg(img, size = 3):
+    return cv2.blur(img,(size, size))
+
+def median(img, size = 3):
+    return cv2.medianBlur(img, size)
+
+def generateDataset(fontSize = 40, imageSize = 50, datasetPath = 'Dataset', labesPath = 'labels.txt'):
 
     all_char_list = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZąęćźżĄĘĆŹŻ!@#$%^&*()_+=-[]\{\};:",.<>/?\''
-
     counter = 0
 
-    fonts_list = []
-    for line in fhandle:
-        fonts_list.append(line.rstrip('\n'))
-    
-    total_fonts = len(fonts_list)
-    all_fonts = glob.glob("C:\\Windows\\Fonts\\*.ttf")
-    f_flag = np.zeros(total_fonts)
-    file = open('label.txt', 'w')
-    
-    for sys_font in all_fonts:
-        font_file = ntpath.basename(sys_font)
-        font_file = font_file.rsplit('.')
-        font_file = font_file[0]
-        f_idx = 0
-        for font in fonts_list:
-            tmp_font = font
-            f_lower = font.lower()
-            s_lower = sys_font.lower()
-            if f_lower in s_lower:
-                path = sys_font
-                font = ImageFont.truetype(path, fontSize)
-                f_flag[f_idx] = 1
-                for ch in all_char_list:
-                    image = Image.new("RGB", imgSize, (255,255,255))
-                    draw = ImageDraw.Draw(image)
-                    pos_x = 0
-                    pos_y = 0
-                    pos_idx=0
-                    for y in [pos_y-1, pos_y, pos_y+1]:
-                        for x in [pos_x-1, pos_x, pos_x+1]:
-                            position = (x,y)
-                            draw.text(position, ch, (0,0,0), font=font)
-                            l_u_d_flag = "u"
-                            if ch.islower():
-                                l_u_d_flag = "l"
-                            elif ch.isdigit():
-                                l_u_d_flag = "d"
+    if not os.path.exists(datasetPath):
+        os.makedirs(datasetPath)
 
-                            file_name = '{}.jpg'.format(counter)
-                            file_name = os.path.join(dataset_path,file_name)
-                            file.write('{} {} {}\n'.format(counter, ch, tmp_font))
-                            image.save(file_name)
-                            counter += 1
-                            pos_idx = pos_idx + 1
-            f_idx = f_idx + 1
+    fileLables = open(labesPath, 'w')
+    fonts = glob.glob("C:\\Windows\\Fonts\\*.ttf")
 
-def generateExtendedDataset():
-    new_labels = open('new_label.txt', 'w')
-    counter = 0
+    ff = open('Fonts_list.txt', 'r')
 
-    labels = open('label.txt', 'r')
-    data = labels.read()
-    data = data.split('\n')
-    contain = {}
-    regex = '([\d]+) (.+) (.+)'
-    p = re.compile(regex)
+    fontList = [x.lower() for x in ff.read().split('\n')]
 
-    for line in data:
-        if line:
-            m = p.match(line)
-            number = m.group(1)
-            char = m.group(2)
-            font = m.group(3)
+    pos = (0,0)
+    for fontName in fonts:
 
-            contain[number] = [char, font]
+        fName = str(fontName).split('\\')[-1].lower()
+        fName = fName.split('.')[0]
 
-    labels.close()
+        if fName not in fontList:
+            continue
 
-    for file in os.listdir('Synthetic_dataset'):
-        name = file.replace('.jpg', '')
-        img = cv2.imread('Synthetic_dataset/' + file, 0)
-        
-        height = []
-        for i in range(0, len(img)):
-            if 0 in img[i]:
-                height.append(i)
+        font = ImageFont.truetype(fontName, fontSize)
 
-        width = []
-        for i in range(0, len(img)):
-            tmp = [row[i] for row in img]
-            if 0 in tmp:
-                width.append(i)
+        for char in all_char_list:
+            image = Image.new('RGB', (imageSize, imageSize), (255, 255, 255))
+            draw = ImageDraw.Draw(image)
+            draw.text(pos, char, (0,0,0), font=font)
 
-        minX = min(height)
-        maxX = max(height)
+            img = np.array(image)
 
-        minY = min(width)
-        maxY = max(width)
+            img = addPadding(img, imageSize)
+            imgName = '{}/{}.jpg'.format(datasetPath, counter)
+            cv2.imwrite(imgName, img)
+            fileLables.write('{} {} {}\n'.format(counter, char, fName))
+            counter += 1
 
-        newImg = img[minX : maxX+2, minY:maxY+2]
-        h,w = newImg.shape[:2]
-        paddingL = (48-w)//2
-        paddingT = (48-h)//2
+            img1 = gausiabBlur(img)
+            imgName = '{}/{}.jpg'.format(datasetPath, counter)
+            cv2.imwrite(imgName, img1)
+            fileLables.write('{} {} {}\n'.format(counter, char, fName))
+            counter += 1
 
-        canvas = np.ones((48,48), np.uint64)
-        canvas[:,:] = (255)
-        canvas[paddingT:paddingT+h, paddingL:paddingL+w] = newImg
+            img1 = avg(img)
+            imgName = '{}/{}.jpg'.format(datasetPath, counter)
+            cv2.imwrite(imgName, img1)
+            fileLables.write('{} {} {}\n'.format(counter, char, fName))
+            counter += 1
 
-        file_name = '{}.jpg'.format(counter)
-        new_labels.write('{} {} {}\n'.format(counter, contain[name][0], contain[name][1]))
-        cv2.imwrite('Synthetic_dataset_new/' + file_name, canvas)
-        img = cv2.imread('Synthetic_dataset_new/' + file_name, 0)
-        counter += 1
+            img1 = median(img)
+            imgName = '{}/{}.jpg'.format(datasetPath, counter)
+            cv2.imwrite(imgName, img1)
+            fileLables.write('{} {} {}\n'.format(counter, char, fName))
+            counter += 1
 
-        img = cv2.imread('Synthetic_dataset_new/' + file_name, 0)
+    fileLables.close()
 
-        file_name = '{}.jpg'.format(counter)
-        new_labels.write('{} {} {}\n'.format(counter, contain[name][0], contain[name][1]))
-        imgBlur = blur(img)
-        cv2.imwrite('Synthetic_dataset_new/' + file_name, imgBlur)
-        counter += 1
+def labelsToBin(txtLables, binLabels):
+    file = open(txtLables, 'r')
+    data = file.read().split('\n')
+    file.close()
+    file = open(binLabels, 'wb')
+    for item in data:
+        item = item + '\n'
+        file.write(item.encode('utf-8'))
 
-        file_name = '{}.jpg'.format(counter)
-        new_labels.write('{} {} {}\n'.format(counter, contain[name][0], contain[name][1]))
-        imgGaussian = gaussian(img)
-        cv2.imwrite('Synthetic_dataset_new/' + file_name, imgGaussian)
-        counter += 1
+    file.close()
 
-        file_name = '{}.jpg'.format(counter)
-        new_labels.write('{} {} {}\n'.format(counter, contain[name][0], contain[name][1]))
-        imgMedian = median(img)
-        cv2.imwrite('Synthetic_dataset_new/' + file_name, imgMedian)
-        counter += 1
+def readBin(binLabels):
+    with open(binLabels, 'rb') as file:
+        data = file.read()
+        data = data.decode('utf-8')
+        data = data.split('\n')
+        for item in data:
+            print(item)
 
-
+if __name__ == '__main__':
+    # generateDataset()
+    # labelsToBin('labels.txt', 'labels.bin')
+    readBin('labels.bin')
