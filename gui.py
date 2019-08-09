6531bbe393baf658ec47972ca54651de76ffc8fa
Paweł Przybyłowski <pawel.przybylowski@outlook.com>
2019-08-09 16:15:06 +0200

    tkinter gui
    hejka

--- gui.py
+++ gui.py
@@ -1,177 +1,141 @@
-import pygame
-import easygui
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+from PIL import ImageTk, Image
 import fileApi 
 import imageProcessing as imgProc
 
-class ocrApp(object):
-    def __init__(self):
-        pygame.init()
-        pygame.display.set_caption('OCR App')
 
+COLOR_WHITE = (247, 247, 247)
+COLOR_BLACK = (59, 89, 152)
+COLOR_BLUE = (0, 0, 255)
+COLOR_RED = (255, 0, 0)
+
+SIZE_WIDTH = 650
+SIZE_HEIGHT = 400
+RESULTS_WIDTH = 768
+RESULTS_HEIGHT = 1024
+
+
+class ResultsWindow:
+    counter = 0
+    def __init__(self, master, images, mode):
+        self.api = fileApi.fileAPI()
+        self.images = images
+        self.master = master
+        self.frame = Frame(self.master)
+        next_button = Button(self.frame, text='Next image', command=self.next_image)
+        previous_button = Button(self.frame, text='Previous image', command=self.previous_image)
+        self.entry_word = Entry(self.frame)
+        find_button = Button(self.frame, text='Find', command=self.find)
+        remove_button = Button(self.frame, text='Remove image', command=self.remove)
+        add_button = Button(self.frame, text='Add image(s)', command=self.add)
+
+        img = Image.open(self.images[self.counter])
+        img.thumbnail((RESULTS_WIDTH - 10, RESULTS_HEIGHT - 120), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        self.panel = Label(self.frame, image=img)
+        self.panel.grid(row=0, columnspan=3, sticky=SW)
+
+        next_button.grid(row=1, column=2)
+        previous_button.grid(row=1, column=0)
+        remove_button.grid(row=2, column=2)
+        add_button.grid(row=2, column=0)
+        self.entry_word.grid(row=3, column=0)
+        find_button.grid(row=3, column=2)
+
+        self.frame.pack()
+        self.frame.mainloop()
+
+    def set_image(self):
+        img = Image.open(self.images[self.counter])
+        img.thumbnail((RESULTS_WIDTH - 10, RESULTS_HEIGHT - 120), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(img)
+        self.panel.configure(image=img)
+        self.panel.image = img
+
+    def next_image(self):
+        self.counter += 1
+        if self.counter > len(self.images) - 1:
+            self.counter = 0
+        self.set_image()
+
+    def previous_image(self):
+        self.counter -= 1
+        if self.counter < 0:
+            self.counter = len(self.images) - 1
+        self.set_image()
+
+    def find(self):
+        print(self.entry_word.get())
+        #szukanie slowa na obrazie
+    
+    def remove(self):
+        self.images.remove(self.images[self.counter])
+        self.counter=0
+        messagebox.showinfo('Info', 'Deleted successfully!')
+
+    def add(self):
+        files = list(filedialog.askopenfilenames(parent=self.master, title='Choose image file(s)', filetypes=(("All files", "*.*"), ("PNG", "*.png"), ("JPG", "*.jpg"), ("Bitmap", "*.bmp"))))
+        if len(files) != 0:
+            messagebox.showinfo('Info', 'Added image(s)!')
+            #fileNames = self.api.readFiles(files)
+            #imgProc.showResizedImage('Result',imgProc.straightenImage(self.api.getImage(fileNames[0])),2)
+            for file in files:
+                self.images.append(file)
+
+
+class UI:
+    def test(self):
+        self.newWindow.grab_release()
+        self.newWindow.destroy()
+
+    root = Tk()
+    var = IntVar()
+    def __init__(self):
         self.api = fileApi.fileAPI()
 
-        self.layout = 1
-        self.WIDTH = 640
-        self.HEIGHT = 480
-        self.WHITE = (255, 255, 255)
-        self.BLACK = (0, 0, 0)
-        self.ACTIVE_COLOR = pygame.Color('dodgerblue1')
-        self.INACTIVE_COLOR = pygame.Color('dodgerblue4')
-        self.buttonFont = pygame.font.Font(None, 30)
-        self.footerFont = pygame.font.Font(None, 16)
-        self.headerFont = pygame.font.Font(None, 50)
+        self.root.maxsize(SIZE_WIDTH, SIZE_HEIGHT)
+        self.root.minsize(SIZE_WIDTH, SIZE_HEIGHT)
+        self.root.resizable(0, 0)
+        self.root.winfo_toplevel().title('OCR App')
+        FONT = 'Microsoft Sans Serif'
+        font_header = (FONT, 45)
+        font_footer = (FONT, 10)
+        header = Label(self.root, text='OCR App', font=font_header)
+        footer = Label(self.root, text='by Kontowicz Piotr, Przybylowski Pawel, Szkudlarek Damian', font=font_footer)
+        header.pack()
+        footer.place(x=10, y=375)
 
-    def draw_app_text(self, screen):
-        screen.blit(self.footerFont.render('wykonane przez: Kontowicz Piotr, Przybyłowski Paweł, Szkudlarek Damian', True, self.BLACK), (10, self.HEIGHT-20))
-
-    def draw_button(self, button, screen):
-        """Draw the button rect and the text surface."""
-        pygame.draw.rect(screen, button['color'], button['rect'])
-        screen.blit(button['text'], button['text rect'])
-
-
-    def create_button(self, x, y, w, h, text, callback):
-        """A button is a dictionary that contains the relevant data.
-
-        Consists of a rect, text surface and text rect, color and a
-        callback function.
-        """
-        text_surf = self.buttonFont.render(text, True, self.WHITE)
-        button_rect = pygame.Rect(x, y, w, h)
-        text_rect = text_surf.get_rect(center=button_rect.center)
-        button = {
-            'rect': button_rect,
-            'text': text_surf,
-            'text rect': text_rect,
-            'color': self.INACTIVE_COLOR,
-            'callback': callback,
-            }
-        return button
-
-    def show_image(self, screen, image, x, y):
-        screen.blit(image, (x,y))
-
-    def main(self):
-        screen = pygame.display.set_mode((640, 480))
-        clock = pygame.time.Clock()
-        done = False
         
-        number = 0
+        r1 = Radiobutton(self.root, text='Faktura', variable = self.var, value = 1)
+        r2 = Radiobutton(self.root, text='Tekst naukowy', variable = self.var, value = 2)
 
-        def load_files():
-            paths = easygui.fileopenbox(title='Choose file', default='*.png', filetypes=['*.png', '*.txt'], multiple=True)
-            if paths != None:
-                fileNames = self.api.readFiles(paths)
-                imgProc.showResizedImage('Result',imgProc.straightenImage(self.api.getImage(fileNames[0])),2)
-                #DO POPRAWY WCZYTYWANIE ŚCIEŻKI
+        r1.pack()
+        r2.pack()
+        load_images_button = Button(self.root, text='Select image(s)', command=self.load_images, width=50, height=5)
+        load_images_button.place(x=145, y=150)
 
-        def save_results():
-            path = easygui.filesavebox()
-            if path != None:
-                self.api.saveResults(path)
+        self.root.mainloop()
 
-        def quit_game():  # A callback function for the button.
-            nonlocal done
-            done = True
+    def show_results_window(self, master, images, var):
+        self.newWindow = Toplevel(master)
+        self.newWindow.winfo_toplevel().title('OCR App - Preview Images')
+        self.newWindow.maxsize(RESULTS_WIDTH, RESULTS_HEIGHT)
+        self.newWindow.minsize(RESULTS_WIDTH, RESULTS_HEIGHT)
+        self.newWindow.resizable(0, 0)
+        self.newWindow.grab_set()
+        self.newWindow.protocol("WM_DELETE_WINDOW", self.test)
+        self.app = ResultsWindow(self.newWindow, images, var)
 
-        def set_layout_1():
-            self.layout = 1
-
-        def set_layout_2():
-            self.layout = 2
-
-        buttonLoadFile = self.create_button(100, 100, 180, 60, 'Load file(s)', load_files)
-        buttonSave = self.create_button(100, 200, 180, 60, 'Save results', save_results)
-        buttonQuit = self.create_button(100, 300, 180, 60, 'Quit', quit_game)
-        menuGap = self.create_button(360, 0, 180, 30, None, None)
-        menuAppName = self.create_button(540, 0, 100, 30, 'OCR App', None)
-
-        layout1 = self.create_button(0, 0, 180, 30, 'Processing', set_layout_1)
-        layout2 = self.create_button(180, 0, 180, 30, 'Verification', set_layout_2)
-
-        buttonLoadImage = self.create_button(100, 100, 180, 60, "Load image file", load_files)
-        buttonLoadTextFile = self.create_button(100, 200, 180, 60, "Load text file", load_files)
-        buttonCompare = self.create_button(100, 300, 180, 60, "Compare files", None)
-
-        # A list that contains all buttons.
-        button_list = [buttonLoadFile, buttonSave, buttonQuit]
-        button_list2 = [buttonLoadImage, buttonLoadTextFile, buttonCompare]
-        button_menu = [layout1, layout2, menuGap, menuAppName]
-
-        while not done:
-
-            if self.layout == 1:
-
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        done = True
-                    elif event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1:
-                            #sprawdzamy czy lewy klik
-                            for button in button_list:
-                                #jesli ta to ktory button
-                                if button['rect'].collidepoint(event.pos):
-                                    #a tutaj callback funkcja batona
-                                    button['callback']()
-                            for button in button_menu:
-                                if button['rect'].collidepoint(event.pos) and button['callback'] is not None:
-                                    button['callback']()
-                    elif event.type == pygame.MOUSEMOTION:
-                        #sprawdzamy jak poruszlismy myszkiem
-                        for button in button_list:
-                            #jak wyjechalismy poza obreb przycisku to zmieniamy kolor na nieaktywny i to samo w 2 strone
-                            if button['rect'].collidepoint(event.pos):
-                                button['color'] = self.ACTIVE_COLOR
-                            else:
-                                button['color'] = self.INACTIVE_COLOR
-                        for button in button_menu:
-                            if button['rect'].collidepoint(event.pos) and button['callback'] != None:
-                                button['color'] = self.ACTIVE_COLOR
-                            else:
-                                button['color'] = self.INACTIVE_COLOR
-                image = pygame.image.load('data/test.png')
-
-                screen.fill(self.WHITE)
-                self.show_image(screen, image, 320, 130)
-                self.draw_app_text(screen)
-                for button in button_list:
-                    self.draw_button(button, screen)
-                for button in button_menu:
-                    self.draw_button(button, screen)
-                pygame.display.update()
-                clock.tick(30)
-            elif self.layout == 2:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        done = True
-                    elif event.type == pygame.MOUSEBUTTONDOWN:
-                        if event.button == 1:
-                            for button in button_menu:
-                                if button['rect'].collidepoint(event.pos) and button['callback'] is not None:
-                                    button['callback']()
-                    elif event.type == pygame.MOUSEMOTION:
-                        for button in button_list2:
-                            if button['rect'].collidepoint(event.pos):
-                                button['color'] = self.ACTIVE_COLOR
-                            else:
-                                button['color'] = self.INACTIVE_COLOR
-                        for button in button_menu:
-                            if button['rect'].collidepoint(event.pos) and button['callback'] != None:
-                                button['color'] = self.ACTIVE_COLOR
-                            else:
-                                button['color'] = self.INACTIVE_COLOR
-
-                screen.fill(self.WHITE)
-                self.draw_app_text(screen)
-                for button in button_list2:
-                    self.draw_button(button, screen)
-                for button in button_menu:
-                    self.draw_button(button, screen)
-                pygame.display.update()
-                clock.tick(30)
+    def load_images(self):
+        files = list(filedialog.askopenfilenames(parent=self.root, title='Choose image file(s)', filetypes=(("All files", "*.*"), ("PNG", "*.png"), ("JPG", "*.jpg"), ("Bitmap", "*.bmp"))))
+        if len(files) != 0:
+            messagebox.showinfo('Info', 'Loaded images!')
+            #fileNames = self.api.readFiles(files)
+            #imgProc.showResizedImage('Result',imgProc.straightenImage(self.api.getImage(fileNames[0])),2)
+            self.show_results_window(self.root, files, self.var)
 
 
 if __name__ == "__main__":
-    app = ocrApp()
-    app.main()
-    pygame.quit()
+    UI()
